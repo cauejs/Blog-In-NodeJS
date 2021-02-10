f0753e37416b09a088874307b372a59c7511d73d
Matheus Ferreira <teteu.andrade06@gmail.com>
2021-02-10 01:15:14 -0300

    Connecting to MongoDB, and form validation

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ const config = require('./config.json');
 const app = express();
 const admin = require('./routes/admin');
 const path = require('path');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 // Configuration
+    // Session
+        app.use(session({
+            secret: config.sessionSecret,
+            resave: true,
+            saveUninitialized: true
+        }));
+        app.use(flash())
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })    
     // BodyParser
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(bodyParser.json())
@@ -17,7 +32,14 @@ const path = require('path');
         app.engine('hbs', hbs({defaultLayout: 'main', extname: "hbs", helpers: {appName: config.appName}}));
         app.set('view engine', 'hbs')
     // Mongoose (DataBase)
-        // Another    
+        mongoose.connect(config.MongoDB, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        }).then(()=>{
+            console.log("Successfully connected to MongoDB");
+        }).catch(err=>{
+            console.error("Error connecting to MongoDB")
+        })
     // Public (css, js, images)
         app.use(express.static(path.join(__dirname,"public")))   
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require("../models/Category")
+const Category = mongoose.model("categories");
 
 // Admin Routes
 
@@ -9,14 +12,56 @@ router.get('/', (req, res) => {
 
 router.get('/posts', (req, res) => {
     res.send("Posts Page");
-})
+});
 
 router.get('/categories', (req, res) => {
     res.render('admin/categories');
-})
+});
 
 router.get('/categories/add', (req, res) => {
     res.render('admin/categoriesAdd');
-})
+});
+
+router.post("/categories/new", (req, res) => {
+
+    var errors = [];
+
+    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
+        errors.push({text: "Name invalid!"})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        errors.push({text: "Slug invalid!"})
+    }
+
+    if(req.body.name.length < 2) {
+        errors.push({text: "The name entered is too small!"})
+    }
+
+    if(/\s/g.test(req.body.slug)) {
+        errors.push({text: "Slug Cannot Contain Spaces!"});
+    }
+    
+    if(/[A-Z]/gm.test(req.body.slug)) {
+        errors.push({text: "Slug Cannot Contain Capital Letters!"});
+    }
+
+    if(errors.length > 0) {
+        res.render("admin/categoriesAdd", {errors})
+    } else {
+        const newCategory = {
+            name: req.body.name,
+            slug: req.body.slug
+        }
+    
+        new Category(newCategory).save().then(() => {
+            req.flash("success_msg", "Category created successfully!")
+            res.redirect("/admin/categories")
+        }).catch(err => {
+            req.flash("error_msg", "There was an error saving the category, try again!")
+            res.redirect("/admin")
+        })
+    }
+});
 
 module.exports = router;
